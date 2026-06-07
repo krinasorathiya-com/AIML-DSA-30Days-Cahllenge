c1e082d377a6fc0e8e2a545a44624babeb245e53
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-07 19:34:53 +0530

    Refactor even number output in even.py for improved readability

--- Day 03/even.py
+++ Day 03/even.py
@@ -4,7 +4,7 @@ num2 = int(input("Enter the second number : "))
 for i in range((num1), (num2) + 1):
     if(i<=0): print("Please enter a positive number")
         
-    elif(i % 2 == 0): print(f"{i}")
-    print("All the even numbers between", num1, "and", num2, "are printed above.")
-    else:print("Please Enter number not string.")
+    elif(i % 2 == 0):
+        print(i)
+    else: print("Please Enter number not string.")
 
