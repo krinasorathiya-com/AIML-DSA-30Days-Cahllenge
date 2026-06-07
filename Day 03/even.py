a27b8197823bd79109a392661ea0c4215e802a02
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-07 19:40:07 +0530

    Fix even number output logic in even.py to ensure correct termination of loop

--- Day 03/even.py
+++ Day 03/even.py
@@ -2,9 +2,9 @@ print(" EVEN number list : ")
 num1 =int(input("Enter the first  number : "))
 num2 = int(input("Enter the second number : "))
 for i in range((num1), (num2) + 1):
-    if(i<=0): print("Please enter a positive number")
         
-    elif(i % 2 == 0):
+    if(i % 2 == 0):
         print(i)
-    else: print("Please Enter number not string.")
+        
+    else: print("Please Enter number valid number .")
 
