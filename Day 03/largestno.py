371fba67ef1d28cb0d6253ba20b0137b40c99dd3
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 18:08:16 +0530

    Add largest digit finder and number analyzer functions

--- Day 03/largestno.py
+++ Day 03/largestno.py
@@ -1,1 +1,11 @@
-num=int(input("Enter the number :"))
+num = int(input("Enter the number: "))
+large = 0
+
+while num > 0:
+    a = num % 10
+  if (a > large):
+        large = a
+
+num = num // 10
+
+print(f"The largest digit in this number is {large}")
---
+++ Day 03/numberanalyzer.py
@@ -0,0 +1,40 @@
+num=int(input("Enter a number: "))
+count=0
+while (num>0):
+    num=num//10
+    count=count+1
+print("Number of digits =", count)
+
+sum = 0
+
+while (n> 0):
+    digit = n % 10
+    sum = sum + digit
+    n = n // 10
+
+print("Sum of digits =", sum)
+ 
+
+
+
+
+
+
+
+reverse = 0
+while(n>0):
+    a=n%10
+    reverse =(reverse*10)+a
+    n=n//10
+print(f"THE REVERSE NUMBER OF THIS NUMBER IS {reverse} .")
+
+original=n
+reverse=0
+while(n>0):
+    a=n%10
+    reverse=(reverse*10)+a
+    n=n//10
+if(original==reverse):
+
+    print("The number is  pallindrome number.")
+else:print("the number is not  pallindrome number.")
