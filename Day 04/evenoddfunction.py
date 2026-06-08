48e270b7acb256e5a52af3489a73ad25ea1de185
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 18:43:31 +0530

    Fix conditional logic in odd function to use elif for clarity

--- Day 04/evenoddfunction.py
+++ Day 04/evenoddfunction.py
@@ -1,9 +1,9 @@
 def odd(num1):
     if(num1%2!=0):
         print(f"The number {num1}  is odd.")
-    if(num1==0):
+    elif(num1==0):
         print("the number is not Even Or odd.")
-    if(num1%2==0):
+    elif(num1%2==0):
         print(f"The  number {num1} is even.")
 odd(13)
 odd(12)
