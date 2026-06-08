62602b6a076774ebdba93fffedd1ac68f97a1238
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 18:37:27 +0530

    Add odd number checker function with output for even, odd, and zero cases

---
+++ Day 04/evenoddfunction.py
@@ -0,0 +1,10 @@
+def odd(num1):
+    if(num1%2==0):
+        print(f"The number {num1}  is Even.")
+    if(num1==0):
+        print("the number is not Even Or odd.")
+    else:
+        print(f"The  number {num1} is odd.")
+odd(13)
+odd(12)
+odd(0)
