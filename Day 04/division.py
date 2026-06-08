482ba3382db2f2fc1ff3b11e260e6a7717f30baa
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 17:29:17 +0530

    Add division function to compute and display the result of division

---
+++ Day 04/division.py
@@ -0,0 +1,10 @@
+def division(num1,num2):
+    a=num1/num2
+    if(num2==0):print("Arithmatic exception :  ")
+    else:
+        print(f"division is:{a}")
+        return a
+division(20,10)
+division(10,20)
+division(-30,90)
+division(90,0)
