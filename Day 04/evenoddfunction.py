96cb71b08f2728758ef42077a6b5537e5c6b3172
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 18:40:21 +0530

    Fix odd/even number logic in odd function

--- Day 04/evenoddfunction.py
+++ Day 04/evenoddfunction.py
@@ -1,10 +1,10 @@
 def odd(num1):
-    if(num1%2==0):
-        print(f"The number {num1}  is Even.")
+    if(num1%2!=0):
+        print(f"The number {num1}  is odd.")
     if(num1==0):
         print("the number is not Even Or odd.")
     else:
-        print(f"The  number {num1} is odd.")
+        print(f"The  number {num1} is even.")
 odd(13)
 odd(12)
 odd(0)
