95d14761d419a479d9304d8e34b34d1b1070df26
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 13:29:57 +0530

    Fix palindrome check logic and improve variable usage in pallindrome.py

--- Day 03/pallindrome.py
+++ Day 03/pallindrome.py
@@ -1,11 +1,12 @@
 n=int(input("Enter the number : "))
+original=n
 reverse=0
 while(n>0):
     a=n%10
     reverse=(reverse*10)+a
     n=n//10
 print(f"The reverse number is: {reverse}")
-if(n==reverse):
+if(original!=reverse):
 
-    print("The number is pallindrome number.")
-else:print("the number is not pallindrome number.")
+    print("The number is not pallindrome number.")
+else:print("the number is  pallindrome number.")
