6ef9dc510a601cabb0a5dc64948d6e3e2df4466f
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 09:22:52 +0530

    Refactor print statement for multiplication table in table.py to use f-string formatting

--- Day 03/table.py
+++ Day 03/table.py
@@ -1,5 +1,5 @@
 print("Multiplication Table")
 no = int(input("Enter the number which multiplication table you want : "))
 for i in range(1, 11):
-    print(no, "x", i, "=", no * i)
+    print(f"{no} x {i} ={no * i}")
     
