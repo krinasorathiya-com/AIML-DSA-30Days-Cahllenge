7368190ed2f697068d7d64800109a0a118970ca9
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 18:29:23 +0530

    Remove unused variable and clean up largest digit finder function

--- Day 03/largestno.py
+++ Day 03/largestno.py
@@ -1,11 +1,7 @@
 num = int(input("Enter the number: "))
-large = 0
 
 while (num > 0):
     a = num % 10
+num = num // 10
 
-    if (a > large):
-     large = a
-     num = num // 10
-print(f"The largest digit in this number is {large}")
 
