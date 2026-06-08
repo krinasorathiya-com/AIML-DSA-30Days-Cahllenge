a271377590d2d856266d70ee4783377b8eff6d84
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-08 17:19:19 +0530

    Add subtraction function to compute the difference between two numbers

---
+++ Day 04/substraction.py
@@ -0,0 +1,9 @@
+def substraction(num1,num2):
+    sub=num1-num2
+    if(sub<0):
+        print(f"sub : {num2}-{num1}")
+        return num2-num1
+    else:
+        return num1-num2
+substraction(20,10)
+substraction(10,20)    
