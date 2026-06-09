96cc3bf345d6417f822d873ba82d0ea5f975dd78
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-09 18:17:44 +0530

    Fix logic to correctly identify and print the largest digit in a number

--- Day 03/largestno.py
+++ Day 03/largestno.py
@@ -1,7 +1,11 @@
 num = int(input("Enter the number: "))
-
+large =0
 while (num > 0):
     a = num % 10
+    if(large<a):
+        print(f"Largest digit is: {a}.")
+    else:
+        print(f"Largest number is : {large}")    
 num = num // 10
 
 
