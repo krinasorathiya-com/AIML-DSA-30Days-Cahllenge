6bd3b24adc383d642d0a0b660c4814e8f1e6e9f3
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-09 18:29:37 +0530

    Refactor number analyzer for consistent use of parentheses in while loops

--- Day 03/numberanalyzer.py
+++ Day 03/numberanalyzer.py
@@ -4,34 +4,34 @@ original = num
 
 count = 0
 temp = num
-while temp > 0:
+while (temp > 0):
     temp = temp // 10
     count += 1
 print("Number of digits =", count)
 
 sum_digits = 0
-temp = num
-while temp > 0:
-    digit = temp % 10
+temp1 = num
+while (temp > 0):
+    digit = temp1 % 10
     sum_digits += digit
-    temp = temp // 10
+    temp1 = temp1 // 10
 print("Sum of digits =", sum_digits)
 
 largest = 0
-temp = num
-while temp > 0:
-    digit = temp % 10
+temp2 = num
+while (temp > 0):
+    digit = temp2 % 10
     if digit > largest:
         largest = digit
-    temp = temp // 10
+    temp2 = temp2 // 10
 print("Largest digit is:", largest)
 
 reverse = 0
-temp = num
-while temp > 0:
-    digit = temp % 10
+temp3 = num
+while (temp > 0):
+    digit = temp3 % 10
     reverse = reverse * 10 + digit
-    temp = temp // 10
+    temp3 = temp3 // 10
 print("Reverse number =", reverse)
 
 if original == reverse:
