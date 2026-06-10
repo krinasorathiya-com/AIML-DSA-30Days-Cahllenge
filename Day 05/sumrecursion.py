21923a041b5d8ba9ee415fa0e956e2b4f78b58e9
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-10 23:30:21 +0530

    Fix formatting and return type in sum_of_digits function

--- Day 05/sumrecursion.py
+++ Day 05/sumrecursion.py
@@ -1,7 +1,8 @@
 def sum_of_digits(n):
-    if n == 0:      
+    if (n == 0):      
         return 0
 
-    return n % 10 + sum_of_digits(n // 10)
+    return [n % 10 + sum_of_digits(n // 10)]
 
 print(sum_of_digits(1234))
+print(sum_of_digits(5643))
