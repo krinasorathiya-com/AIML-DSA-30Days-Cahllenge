6e6d01deab17c0ba24a706ceb9fb05323ac5d06b
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-10 19:30:06 +0530

    Refactor conditional statements in smallest number function for consistency

--- Day 05/smallestno.py
+++ Day 05/smallestno.py
@@ -1,9 +1,9 @@
 def small(a,b,c):
     if(a<c or a<b):
         print(f"{a} is smallest number in this all number.")
-    elif(b<c or b<a):
+    if(b<c or b<a):
         print(f"{b} is the smallest number in this all number.")
-    elif(c<a or c<b):
+    if(c<a or c<b):
         print(f"{c} is smallest number in this all number.")
 
 small(-99,23,56)
