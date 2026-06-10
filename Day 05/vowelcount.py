026197bc31efe7fe78b53359083f317bbf2f4e50
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-10 20:08:31 +0530

    Add vowel count function with initial implementation

---
+++ Day 05/vowelcount.py
@@ -0,0 +1,16 @@
+def vowelcount(a):
+    count =0
+    if(a.count(a)):
+        count+=1
+    if(a.count(e)):
+        count+=1    
+    if(a.count(i)):
+        count+=1
+    if(a.count(o)):
+        count+=1
+    if(a.count(u)):
+        count+=1
+    else:
+        count=0
+vowelcount("Education")        
+
