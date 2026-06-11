ed9b1275ff6cd8e53480c73fab8ade2f4777c8cd
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-11 22:39:35 +0530

    Fix syntax error in while loop condition and if statement in Armstrong number check

--- Day 06/armstrong.py
+++ Day 06/armstrong.py
@@ -3,12 +3,12 @@ num = int(input("Enter a number: "))
 temp = num
 sum = 0
 
-while (temp > 0:
+while (temp > 0):
     digit = temp % 10
     sum = sum + digit**3
     temp = temp // 10
 
-if sum == num:
+if (sum == num):
     print("Armstrong Number")
 else:
     print("Not Armstrong Number")
