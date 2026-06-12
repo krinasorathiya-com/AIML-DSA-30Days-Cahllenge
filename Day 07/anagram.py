175eef8568f0fe566fade3e23dc16091eb32c917
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-12 21:52:43 +0530

    Fix anagram check to compare lowercase versions of input strings

--- Day 07/anagram.py
+++ Day 07/anagram.py
@@ -1,7 +1,7 @@
 str1 = input("Enter first string: ")
 str2 = input("Enter second string: ")
 
-if (sorted(str1) == sorted(str2)):
+if (sorted(str1.islower()) == sorted(str2.islower())):
     print("Anagram")
 else:
     print("Not Anagram")
