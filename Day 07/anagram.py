8fd7b45a2041a4901558206de7f05af1bfc4464d
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-12 21:57:18 +0530

    Update output message for non-anagram case to include input strings

--- Day 07/anagram.py
+++ Day 07/anagram.py
@@ -4,4 +4,4 @@ str2 = input("Enter second string: ").lower()
 if sorted(str1) == sorted(str2):
     print(f"{str1} and {str2} is Anagram.")
 else:
-    print("Not Anagram")
+    print(f"{str1} and {str2} is Not Anagram.")
