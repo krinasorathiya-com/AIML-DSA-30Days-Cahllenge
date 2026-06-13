42a47fd0ab67bd6876c0ef952152f4c4789b9fe3
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-13 21:01:08 +0530

    Refactor character frequency script for improved readability and structure

--- Day 08/frequency.py
+++ Day 08/frequency.py
@@ -1,8 +1,12 @@
 text = input("Enter a string: ")
 
+visited = ""
+
 for ch in text:
-    count = 0
-    for c in text:
-        if (ch == c):
-            count += 1
-    print(ch, "=", count)
+    if ch not in visited:
+        count = 0
+        for c in text:
+            if ch == c:
+                count += 1
+        print(ch, "=", count)
+        visited += ch
