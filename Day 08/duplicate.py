7f01186acbc57c7d150229eb67c5480050f96adf
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-13 20:48:47 +0530

    Add script to identify and print duplicate elements in a list

---
+++ Day 08/duplicate.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 8, 4, 5, 1]
+
+print("Duplicate Elements:")
+
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] == numbers[j]:
+            print(numbers[i])
+            break
