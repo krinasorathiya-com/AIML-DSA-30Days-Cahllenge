bbec020977ec6450445a35bf547edc8eb7c41729
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-13 20:42:33 +0530

    Improve output formatting for even and odd number counts

--- Day 08/listoddeven.py
+++ Day 08/listoddeven.py
@@ -1,13 +1,12 @@
 numbers = [1, 2, 3, 4, 5, 6]
-
 even = 0
 odd = 0
 
 for num in numbers:
-    if num % 2 == 0:
+    if (num % 2 == 0):
         even += 1
     else:
         odd += 1
 
-print("Even =", even)
-print("Odd =", odd)
+print("Even number is =", even)
+print("Odd number is =", odd)
