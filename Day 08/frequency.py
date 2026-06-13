2610499bd71c735d377826b91288c5e2e1c183d4
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-13 21:01:38 +0530

    Refactor frequency counting logic for consistent formatting and improved readability

--- Day 08/frequency.py
+++ Day 08/frequency.py
@@ -3,10 +3,10 @@ text = input("Enter a string: ")
 visited = ""
 
 for ch in text:
-    if ch not in visited:
+    if (ch not in visited):
         count = 0
         for c in text:
-            if ch == c:
+            if (ch == c):
                 count += 1
         print(ch, "=", count)
         visited += ch
