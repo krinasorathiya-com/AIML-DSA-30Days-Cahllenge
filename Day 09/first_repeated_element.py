24b688c634f1e3018bec8f867c049af865e0e867
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-14 21:37:54 +0530

    Improve readability by formatting condition check for first repeated element

--- Day 09/first_repeated_element.py
+++ Day 09/first_repeated_element.py
@@ -3,6 +3,7 @@ numbers = [5, 3, 1, 4, 3, 8]
 visited = set()
 
 for num in numbers:
-    if (num in visited): print("First Repeated Element:", num)
-    break
+    if (num in visited):
+         print("First Repeated Element:", num)
+         break
     visited.add(num)
