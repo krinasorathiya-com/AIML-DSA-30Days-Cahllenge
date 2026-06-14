dc8192b2d38c6c7e8a25999442b2023648545551
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-14 21:35:53 +0530

    Fix syntax error by adding missing parenthesis in condition check

--- Day 09/first_repeated_element.py
+++ Day 09/first_repeated_element.py
@@ -3,7 +3,7 @@ numbers = [5, 3, 1, 4, 3, 8]
 visited = set()
 
 for num in numbers:
-    if (num in visited:
+    if (num in visited):
         print("First Repeated Element:", num)
         break
     visited.add(num)
