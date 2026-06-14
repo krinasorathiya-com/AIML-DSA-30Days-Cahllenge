9d8151e12970733f66a3119ab6e5b9f33a486cf3
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-14 20:34:30 +0530

    Add initial script to count frequency of elements in a list

---
+++ Day 09/count_frequency_of_element.py
@@ -0,0 +1,12 @@
+elements = [1, 2, 2, 3, 1, 4, 2]
+
+frequency = {}
+
+for item in elements:
+    if item in frequency:
+        frequency[item] += 1
+    else:
+        frequency[item] = 1
+
+for key, value in frequency.items():
+    print(f"{key} → {value}")
