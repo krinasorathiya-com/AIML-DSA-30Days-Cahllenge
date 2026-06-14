1c8be5813ecb777dd520adda3ba294961fb3306a
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-14 21:20:45 +0530

    Refactor frequency counting logic for consistent formatting

--- Day 09/count_frequency_of_element.py
+++ Day 09/count_frequency_of_element.py
@@ -1,9 +1,8 @@
 elements = [1, 2, 2, 3, 1, 4, 2]
-
 frequency = {}
 
 for item in elements:
-    if item in frequency:
+    if (item in frequency):
         frequency[item] += 1
     else:
         frequency[item] = 1
