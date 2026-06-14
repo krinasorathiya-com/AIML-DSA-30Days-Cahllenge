5f0fa07f885c9525b69f0e79b99b71c2efb5204d
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-14 21:23:13 +0530

    Add script to identify unique elements in a list

---
+++ Day 09/unique-element.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+frequency = {}
+
+for num in numbers:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+
+print("Unique Elements:")
+
+for key, value in frequency.items():
+    if value == 1:
+        print(key, end=" ")
