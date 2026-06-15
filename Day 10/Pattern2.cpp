fc871c961594d2cb19c3618866f7e15815821b9f
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-15 23:09:59 +0530

    Refactor pattern printing logic to display a right-angled triangle of asterisks

--- Day 10/Pattern2.cpp
+++ Day 10/Pattern2.cpp
@@ -1,17 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int i,j;
-    for(i=1;i<6;i++)
-    {
-        for(j=1;j<6;j++)
-        {
-            if(i==j)
-            cout<<"*";
-            else
-            cout<<" ";
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
         }
+        cout << endl;
     }
+
     return 0;
 }
