93b3166769bdc6520c4ecd5439601ad5812a5cb2
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-16 21:34:52 +0530

    Refactor code structure for improved readability and maintainability

--- Day 11/DSA_Pattern5.cpp
+++ Day 11/DSA_Pattern5.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
     int i,j;
-    for(i=1;i<6;i++)
+    for(i=5;i<0;i++)
     {
-        for(j=1;j<6;j++)
+        for(j=1;j<=i;j++)
         {
-          cout<<" "
+          cout<<"*";
         }
-        cout<<"*"
+        cout<<endl;
     }
 }
