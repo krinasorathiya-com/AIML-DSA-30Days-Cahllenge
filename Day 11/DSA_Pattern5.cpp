1aeef1d48d53e0cc42d11ef30c6505fe54b211f5
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-16 21:36:21 +0530

    Fix formatting and logic errors in pattern printing program

--- Day 11/DSA_Pattern5.cpp
+++ Day 11/DSA_Pattern5.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int i,j;
-    for(i=5;i<0;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-          cout<<"*";
+
+int main() {
+
+    for (int i = 5; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
+
+    return 0;
 }
