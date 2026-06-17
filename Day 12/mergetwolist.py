bb267f588e6afc0ac4a0141868b7be233dce9ae3
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-17 21:59:07 +0530

    Add implementation to merge two lists while removing duplicates

---
+++ Day 12/mergetwolist.py
@@ -0,0 +1,14 @@
+list1 = [1, 2, 3]
+list2 = [3, 4, 5]
+
+result = []
+
+for item in list1:
+    if item not in result:
+        result.append(item)
+
+for item in list2:
+    if item not in result:
+        result.append(item)
+
+print(result)
