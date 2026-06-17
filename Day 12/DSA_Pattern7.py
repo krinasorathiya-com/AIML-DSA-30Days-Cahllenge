c9c0430cde449c20a00d395a5d9d3c43c1c27df4
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-17 22:24:01 +0530

    Add implementation of pyramid pattern in DSA_Pattern7.py

---
+++ Day 12/DSA_Pattern7.py
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 5; i >= 1; i--) {
+        for (int j = 0; j < 5 - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
