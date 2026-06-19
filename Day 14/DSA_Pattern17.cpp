00f470c5a037928e15b5465766025ad1465021e7
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-19 23:03:25 +0530

    Remove commented sections for increasing and decreasing letters in DSA_Pattern17.cpp

--- Day 14/DSA_Pattern17.cpp
+++ Day 14/DSA_Pattern17.cpp
@@ -5,13 +5,9 @@ int main() {
     int n = 4;
 
     for (int i = 1; i <= n; i++) {
-
-        // Increasing letters
         for (char ch = 'A'; ch < 'A' + i; ch++) {
             cout << ch;
         }
-
-        // Decreasing letters
         for (char ch = 'A' + i - 2; ch >= 'A'; ch--) {
             cout << ch;
         }
