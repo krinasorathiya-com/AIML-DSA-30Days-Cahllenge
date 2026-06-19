c74db6746bd8c47d50ba5e961003f82bef3b34e1
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-19 22:59:48 +0530

    Remove unused variable 'n' and simplify loop condition in DSA_Pattern15.cpp

--- Day 14/DSA_Pattern15.cpp
+++ Day 14/DSA_Pattern15.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main() {
-    int n = 5;
-
-    for (int i = n; i >= 1; i--) {
+for (int i = 5; i >= 1; i--) {
         char ch = 'A';
         for (int j = 1; j <= i; j++) {
             cout << ch;
