ee3d8fe7bb62a18037d0b66c6a3bcce920ae0bed
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-20 22:29:54 +0530

    Add longest_wordinfile.py to find the longest word in sample.txt

---
+++ Day 15/longest_wordinfile.py
@@ -0,0 +1,15 @@
+file = open("sample.txt", "r")
+
+text = file.read()
+
+words = text.split()
+
+longest = ""
+
+for word in words:
+    if len(word) > len(longest):
+        longest = word
+
+file.close()
+
+print("Longest word:", longest)
