a876bfe171a7878059f692f2f4ba29a73eee332e
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-21 23:22:38 +0530

    Add frequent element finder to identify the most frequent number in a list

---
+++ Day 16/frequent.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 2, 3, 1, 2, 4]
+frequency = {}
+
+for num in numbers:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+
+most_frequent = None
+max_count = 0
+
+for key, value in frequency.items():
+    if value > max_count:
+        max_count = value
+        most_frequent = key
+
+print("Most Frequent Element:", most_frequent)
