5f81e29671d5bca0dc8a02c4a864d620e96a92e7
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-21 23:05:28 +0530

    Add sumusing_recursion.py to calculate the sum of digits in a number

---
+++ Day 16/sumusing_recursion.py
@@ -0,0 +1,8 @@
+def sum_digits(n):
+    if n == 0:
+        return 0
+    return (n % 10) + sum_digits(n // 10)
+
+number = int(input("Enter a number: "))
+
+print("Sum of digits:", sum_digits(number))
