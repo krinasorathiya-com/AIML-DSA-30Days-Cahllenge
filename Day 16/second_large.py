5ec0fda8ae3a042d048a2d66777b4ae68f474e7e
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-21 23:08:14 +0530

    Fix formatting in second_large.py for consistency

--- Day 16/second_large.py
+++ Day 16/second_large.py
@@ -3,10 +3,10 @@ numbers = [10, 25, 8, 40, 15]
 largest = second = float('-inf')
 
 for num in numbers:
-    if num > largest:
+    if (num > largest):
         second = largest
         largest = num
-    elif num > second and num != largest:
+    elif (num > second and num != largest):
         second = num
 
 print("Second Largest Element:", second)
