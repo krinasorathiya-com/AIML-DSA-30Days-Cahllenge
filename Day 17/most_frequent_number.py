30bc99361f0b391dfbe480b5cb59bbacebb43df2
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-22 22:33:13 +0530

    Add script to find the most frequent number in a list

---
+++ Day 17/most_frequent_number.py
@@ -0,0 +1,18 @@
+numbers = [78,98,78,87,78,56,34,43,12,21,89,78,78]
+frequency = {}
+
+for num in numbers:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+
+max_count = 0
+answer = 0
+
+for key, value in frequency.items():
+    if value > max_count:
+        max_count = value
+        answer = key
+
+print("Most Frequent Number =", answer)
