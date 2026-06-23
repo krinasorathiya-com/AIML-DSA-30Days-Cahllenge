b634ae26c43963099e93330655a864aaa7b6f478
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-23 13:22:13 +0530

    Add function to find the most frequent word in a list

---
+++ Day 18/most_frequent_word.py
@@ -0,0 +1,13 @@
+def most_frequent(words):
+    freq = {}
+    for word in words:
+        freq[word] = freq.get(word, 0) + 1
+    max_word = None
+    max_count = 0
+    for word, count in freq.items():
+        if count > max_count:
+            max_count = count
+            max_word = word
+    return max_word
+
+print(most_frequent(["AI","ML","AI","Python","AI","ML"]))
