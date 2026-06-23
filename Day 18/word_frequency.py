5c7cbae486c05184a37737bd80adb74d518e2f5a
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-23 13:31:48 +0530

    Remove unnecessary newline in word_frequency function

--- Day 18/word_frequency.py
+++ Day 18/word_frequency.py
@@ -4,5 +4,4 @@ def word_frequency(sentence):
         freq[word] = freq.get(word, 0) + 1
     for word, count in freq.items():
         print(f"{word} → {count}")
-
 word_frequency("Python is fun, Python is powerful.")
