1589257767384dc7564d32dce6dc031db2a3aed2
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-24 21:32:39 +0530

    Add group_even_odd.py to categorize numbers into even and odd groups

---
+++ Day 19/group_even_odd.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+groups = {"Even": [], "Odd": []}
+
+for num in numbers:
+    if num % 2 == 0:
+        groups["Even"].append(num)
+    else:
+        groups["Odd"].append(num)
+
+print(groups)
