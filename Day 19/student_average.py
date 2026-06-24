60c9e7f669103d2cea28fb0ad1c8423b61eec77f
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-24 21:27:03 +0530

    Remove unnecessary newline in student_average.py

--- Day 19/student_average.py
+++ Day 19/student_average.py
@@ -1,5 +1,4 @@
 marks = {"Aman": [80,90,85], "Priya": [95,88,92], "Rahul": [70,75,80]}
-
 for student, scores in marks.items():
     avg = sum(scores) / len(scores)
     print(f"{student} → {avg:.2f}")
