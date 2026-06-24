3923aacc514bf54a4c49e5b4621630c3becc788f
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-24 21:30:16 +0530

    Add word_with_frequency.py to count word occurrences in a string

---
+++ Day 19/word_with-frequency.py
@@ -0,0 +1,8 @@
+text = "Python is great and Python is easy"
+
+freq = {}
+for word in text.lower().split():
+    freq[word] = freq.get(word, 0) + 1
+
+for word, count in freq.items():
+    print(f"{word} → {count}")
