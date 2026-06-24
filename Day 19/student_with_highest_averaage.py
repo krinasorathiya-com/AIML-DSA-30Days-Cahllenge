eb8eb4480bd0b3ec57d4fcb7d731d73968c93583
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-24 21:28:20 +0530

    Remove unnecessary newlines in student_with_highest_average.py

--- Day 19/student_with_highest_averaage.py
+++ Day 19/student_with_highest_averaage.py
@@ -1,8 +1,6 @@
 marks = {"Aman": [80,90,85], "Priya": [95,88,92], "Rahul": [70,75,80]}
-
 best_student = None
 best_avg = 0
-
 for student, scores in marks.items():
     avg = sum(scores) / len(scores)
     if avg > best_avg:
