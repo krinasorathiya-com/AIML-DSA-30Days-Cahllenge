4b0889bce7bd1f05fc63a68b055e0c46fbe4aafe
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-25 23:08:09 +0530

    Fix formatting of conditional statement in duplicate.py

--- Day 20/duplicate.py
+++ Day 20/duplicate.py
@@ -5,5 +5,5 @@ for word in words:
     frequency[word] = frequency.get(word, 0) + 1
 print("Duplicate Words:")
 for word, count in frequency.items():
-    if count > 1:
+    if (count > 1):
         print(word)
