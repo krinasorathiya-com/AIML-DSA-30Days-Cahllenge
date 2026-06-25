b778e562ebf1161ade7e88dfefae3bf6428d09e4
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-25 23:07:25 +0530

    Remove unnecessary blank lines in duplicate.py

--- Day 20/duplicate.py
+++ Day 20/duplicate.py
@@ -1,11 +1,8 @@
 sentence = "AI is amazing AI is powerful"
-
 words = sentence.split()
 frequency = {}
-
 for word in words:
     frequency[word] = frequency.get(word, 0) + 1
-
 print("Duplicate Words:")
 for word, count in frequency.items():
     if count > 1:
