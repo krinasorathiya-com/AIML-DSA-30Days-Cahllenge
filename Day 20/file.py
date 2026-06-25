4f7b3344801a8575efd0c04be912f20666eb2a49
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-25 23:22:00 +0530

    Remove unnecessary blank lines in file.py for cleaner code

--- Day 20/file.py
+++ Day 20/file.py
@@ -1,12 +1,9 @@
 file = open("dscf.txt", "r")
-
 text = file.read()
 file.close()
 
 words = text.split()
-
 frequency = {}
-
 for word in words:
     frequency[word] = frequency.get(word, 0) + 1
 print("Word Frequencies:")
