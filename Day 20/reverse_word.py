93795c98826029323945228a3021ae5c1072857f
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-25 23:10:36 +0530

    Add script to reverse words in a sentence in reverse_word.py

---
+++ Day 20/reverse_word.py
@@ -0,0 +1,12 @@
+sentence = "Machine Learning Is Fun"
+
+words = sentence.split()
+
+reversed_words = []
+
+for word in words:
+    reversed_words.append(word[::-1])
+
+result = " ".join(reversed_words)
+
+print(result)
