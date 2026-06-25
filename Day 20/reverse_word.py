a9a008cc635a23edfb159522a771d49b99344a08
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-25 23:14:36 +0530

    Remove unnecessary blank lines in reverse_word.py

--- Day 20/reverse_word.py
+++ Day 20/reverse_word.py
@@ -1,9 +1,7 @@
 sentence = "Machine Learning Is Fun"
 words = sentence.split()
 reversed_words = []
-
 for word in words:
     reversed_words.append(word[::-1])
-
 result = " ".join(reversed_words)
 print(result)
