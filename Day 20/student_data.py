ccd853b50f11efb2d242b5326525f8efe9f9c8a0
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-25 23:05:37 +0530

    Remove unnecessary blank lines in student_data.py

--- Day 20/student_data.py
+++ Day 20/student_data.py
@@ -1,12 +1,9 @@
 def average_marks(marks):
     return sum(marks) / len(marks)
-
 def highest_marks(marks):
     return max(marks)
-
 def lowest_marks(marks):
     return min(marks)
-
 def calculate_grade(avg):
     if avg >= 90:
         return "A"
@@ -16,13 +13,11 @@ def calculate_grade(avg):
         return "C"
     else:
         return "D"
-
 student = {
     "name": "Krina",
     "marks": [85, 90, 78, 88]
 }
 avg = average_marks(student["marks"])
-
 print("Student Name:", student["name"])
 print("Average Marks:", avg)
 print("Highest Marks:", highest_marks(student["marks"]))
