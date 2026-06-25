7ddf771bf0691e1b80a33ea937cdcc1bf4fca26c
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-25 23:51:26 +0530

    Add a blank line for improved readability in duplicate.py

--- Day 20/duplicate.py
+++ Day 20/duplicate.py
@@ -1,6 +1,7 @@
 sentence = "AI is amazing AI is powerful"
 words = sentence.split()
 frequency = {}
+
 for word in words:
     frequency[word] = frequency.get(word, 0) + 1
 print("Duplicate Words:")
