4500e767a5d61dab7284c28a4ca1f25499085d2a
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-26 21:51:34 +0530

    Add all_duplicate-no.py to identify duplicate numbers in a list

---
+++ Day 21/all_duplicate-no.py
@@ -0,0 +1,15 @@
+numbers = [2, 5, 7, 2, 4, 5, 8, 2]
+
+frequency = {}
+
+for num in numbers:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+
+print("Duplicate Numbers:")
+
+for key, value in frequency.items():
+    if value > 1:
+        print(key)
