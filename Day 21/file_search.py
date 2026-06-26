92f3b12f4efb6a50f4772d5e7442ae3651aa32ea
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-26 22:06:12 +0530

    Add file_search.py to implement word searching functionality in a text file

---
+++ Day 21/file_search.py
@@ -0,0 +1,11 @@
+word = input("Enter word to search: ")
+file = open("sample.txt", "r")
+
+content = file.read()
+
+file.close()
+
+if word.lower() in content.lower():
+    print("Word Found")
+else:
+    print("Word Not Found")
