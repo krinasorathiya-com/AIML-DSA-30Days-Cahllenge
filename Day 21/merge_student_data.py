ff30a620aee006ebf09e26c9a95b61d828b240df
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-26 22:00:03 +0530

    Add merge_student_data.py to merge and display student data

---
+++ Day 21/merge_student_data.py
@@ -0,0 +1,19 @@
+student1 = {
+    "Name": "Aman",
+    "Age": 20,
+    "Marks": 85
+}
+
+student2 = {
+    "Name": "Priya",
+    "Age": 21,
+    "Marks": 92
+}
+
+students = []
+
+students.append(student1)
+students.append(student2)
+
+for student in students:
+    print(student)
