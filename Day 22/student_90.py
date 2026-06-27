5364c1ce1cd8db63a990f9276fa63ec31873f61c
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-27 22:34:40 +0530

    Improve readability by adding parentheses in attendance check condition

--- Day 22/student_90.py
+++ Day 22/student_90.py
@@ -2,5 +2,5 @@ attendance = {"Aman": 92,"Priya": 85,"Rahul": 74,"Riya": 98}
 print("Students with attendance above 90%:")
 
 for student, percent in attendance.items():
-    if percent > 90:
+    if (percent > 90):
         print(student)
