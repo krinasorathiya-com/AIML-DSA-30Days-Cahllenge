9674ccdf5d183a3de802510f2be21d1c366c1ece
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-27 22:42:56 +0530

    Remove unnecessary blank lines in duplicate.py for cleaner code

--- Day 22/duplicate.py
+++ Day 22/duplicate.py
@@ -1,10 +1,8 @@
 sentence = "AI AI Machine Learning Learning AI"
 words = sentence.split()
-
 seen = set()
 
 print("Words without duplicates:")
-
 for word in words:
     if word not in seen:
         print(word)
