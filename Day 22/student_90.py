5b7fbeec9b61fca2e9ae2b7f15f7e5afd2be36c5
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-27 22:32:01 +0530

    Add student attendance tracking script for Day 22 challenge

---
+++ Day 22/student_90.py
@@ -0,0 +1,12 @@
+attendance = {
+    "Aman": 92,
+    "Priya": 85,
+    "Rahul": 74,
+    "Riya": 98
+}
+
+print("Students with attendance above 90%:")
+
+for student, percent in attendance.items():
+    if percent > 90:
+        print(student)
