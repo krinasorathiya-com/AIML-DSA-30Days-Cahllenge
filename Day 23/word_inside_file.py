a87c533919c339a5c3cec468c5f5382468d467dc
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-28 23:10:34 +0530

    Add word counting functionality in word_inside_file.py

---
+++ Day 23/word_inside_file.py
@@ -0,0 +1,14 @@
+word = "Python"
+
+with open("sample.txt", "r") as file:
+    text = file.read()
+
+words = text.split()
+
+count = 0
+
+for w in words:
+    if w == word:
+        count += 1
+
+print("Total Occurrences:", count)
