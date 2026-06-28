1e62b30501f7c14791bc39fd2b9e8af430173518
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-28 19:52:31 +0530

    Add count_email.py to implement email domain counting functionality

---
+++ Day 23/count_email.py
@@ -0,0 +1,21 @@
+emails = [
+    "aman@gmail.com",
+    "riya@yahoo.com",
+    "yug@gmail.com",
+    "raj@outlook.com"
+]
+
+domains = {}
+
+for email in emails:
+    domain = email.split("@")[1]
+
+    if domain in domains:
+        domains[domain] += 1
+    else:
+        domains[domain] = 1
+
+print("Domain Counts:")
+
+for domain, count in domains.items():
+    print(domain, "->", count)
