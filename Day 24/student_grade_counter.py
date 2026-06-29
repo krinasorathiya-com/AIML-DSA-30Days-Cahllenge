8e2c7416ecf944d84f63ef923cdf411e7f4c4c89
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-29 17:48:15 +0530

    Add student_grade_counter.py for Day 24 challenge implementation

---
+++ Day 24/student_grade_counter.py
@@ -0,0 +1,15 @@
+grades = {
+    "Aman": "A",
+    "Rahul": "B",
+    "Priya": "A",
+    "Riya": "C",
+    "Neha": "B"
+}
+
+count = {}
+
+for grade in grades.values():
+    count[grade] = count.get(grade, 0) + 1
+
+for grade, total in count.items():
+    print(grade, "→", total)
