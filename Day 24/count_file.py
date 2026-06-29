c496d852a726596ca054da24c042a17e2c18d7a6
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-29 17:40:28 +0530

    Add count_file.py for Day 24 challenge implementation

---
+++ Day 24/count_file.py
@@ -0,0 +1,10 @@
+files = ["notes.pdf", "resume.pdf", "photo.jpg", "data.csv", "image.jpg"]
+
+frequency = {}
+
+for file in files:
+    extension = file.split(".")[-1]
+    frequency[extension] = frequency.get(extension, 0) + 1
+
+for ext, count in frequency.items():
+    print(ext, "→", count)
