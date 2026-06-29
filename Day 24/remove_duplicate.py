e57fee6e49c1dc8e801ee4d3c4f28ba8aa674d83
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-29 17:30:59 +0530

    Implement remove_duplicate.py for Day 24 challenge

---
+++ Day 24/remove_duplicate.py
@@ -0,0 +1,11 @@
+students = ["Aman", "Priya", "Aman", "Rahul", "Priya", "Riya"]
+
+unique = []
+seen = set()
+
+for name in students:
+    if name not in seen:
+        unique.append(name)
+        seen.add(name)
+
+print("Unique Student Names:", unique)
