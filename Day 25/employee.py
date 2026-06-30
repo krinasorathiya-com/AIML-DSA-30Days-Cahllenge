7ad4ed53786211e6b52a2cf7fc0ad9869b89da3b
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-30 21:48:14 +0530

    Add employee.py for Day 25 challenge implementation

---
+++ Day 25/employee.py
@@ -0,0 +1,21 @@
+employees = [
+    ("Aman", "HR"),
+    ("Rahul", "IT"),
+    ("Priya", "IT"),
+    ("Neha", "HR"),
+    ("Riya", "Finance")
+]
+
+def department_counter(data):
+    count = {}
+
+    for name, dept in data:
+        if dept in count:
+            count[dept] += 1
+        else:
+            count[dept] = 1
+
+    for dept, total in count.items():
+        print(dept, "->", total)
+
+department_counter(employees)
