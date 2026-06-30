712580e587b99eb5a85723c10ed5554ec76eee8c
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-30 21:53:27 +0530

    Add file_analysis function to find the longest word and total characters in a file

---
+++ Day 25/longest_word.py
@@ -0,0 +1,18 @@
+def file_analysis(filename):
+    file = open(filename, "r")
+
+    text = file.read()
+    file.close()
+
+    words = text.split()
+
+    longest = ""
+
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+
+    print("Longest Word:", longest)
+    print("Total Characters:", len(text))
+
+file_analysis("sample.txt")
