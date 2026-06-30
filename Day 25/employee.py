ebc71febea1b6564593c2a7c7a80d1c099f5c80f
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-06-30 21:49:55 +0530

    Remove unnecessary blank line in employee.py for improved code clarity

--- Day 25/employee.py
+++ Day 25/employee.py
@@ -2,7 +2,6 @@ employees = [ ("Aman", "HR"), ("Rahul", "IT"), ("Priya", "IT"), ("Neha", "HR"), 
 
 def department_counter(data):
     count = {}
-
     for name, dept in data:
         if dept in count:
             count[dept] += 1
