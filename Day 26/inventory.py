f4a043fde27d07aa39eaa68992c70578983246da
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-01 21:33:51 +0530

    Add inventory management functionality to track available and out of stock products

---
+++ Day 26/inventory.py
@@ -0,0 +1,16 @@
+inventory = {"Laptop": 5, "Keyboard": 0,"Mouse": 8,"Monitor": 2,"Headphones": 0}
+
+available = []
+out_of_stock = []
+total = 0
+
+for product, qty in inventory.items():
+    if qty > 0:
+        available.append(product)
+        total += qty
+    else:
+        out_of_stock.append(product)
+
+print("Available Products:", available)
+print("Out of Stock Products:", out_of_stock)
+print("Total Available Items:", total)
