c780850f68b4a17ac833f68422e1dd46322388ec
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-01 21:37:06 +0530

    Remove unnecessary blank line in longest.py for improved readability

--- Day 26/longest.py
+++ Day 26/longest.py
@@ -1,7 +1,6 @@
 words = ["artificial","AI", "machine", "learning","python","development"]
 
 words.sort(key=len, reverse=True)
-
 print("Top 3 Longest Words:")
 for word in words[:3]:
     print(word)
