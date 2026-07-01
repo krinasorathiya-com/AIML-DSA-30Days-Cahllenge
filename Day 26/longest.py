9a3c9ce6346146cf45e66b4b0eb2efc2d2e9a662
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-01 21:36:44 +0530

    Add implementation of longest words finder in longest.py

---
+++ Day 26/longest.py
@@ -0,0 +1,7 @@
+words = ["artificial","AI", "machine", "learning","python","development"]
+
+words.sort(key=len, reverse=True)
+
+print("Top 3 Longest Words:")
+for word in words[:3]:
+    print(word)
