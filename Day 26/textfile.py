97ec8ff64ae39a401ec0170e92542cc4c868684d
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-01 21:47:23 +0530

    Add file_statistics function to analyze text file contents

---
+++ Day 26/textfile.py
@@ -0,0 +1,21 @@
+def file_statistics(filename):
+    file = open(filename, "r")
+
+    text = file.read()
+    file.close()
+
+    lines = text.split("\n")
+    words = text.split()
+
+    longest = ""
+
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+
+    print("Total Lines:", len(lines))
+    print("Total Words:", len(words))
+    print("Total Characters:", len(text))
+    print("Longest Word:", longest)
+
+file_statistics("sample.txt")
