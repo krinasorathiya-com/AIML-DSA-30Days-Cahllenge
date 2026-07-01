ff73323a061db701171fa667198c49a9fc1bd613
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-01 21:40:42 +0530

    Add email counting implementation in email.py for Day 26 challenge

---
+++ Day 26/email.py
@@ -0,0 +1,20 @@
+emails = [
+    "abc@gmail.com",
+    "xyz@gmail.com",
+    "user@yahoo.com",
+    "dev@outlook.com",
+    "code@gmail.com"
+]
+
+domains = {}
+
+for email in emails:
+    domain = email.split("@")[1]
+
+    if domain in domains:
+        domains[domain] += 1
+    else:
+        domains[domain] = 1
+
+for domain, count in domains.items():
+    print(domain, "->", count)
