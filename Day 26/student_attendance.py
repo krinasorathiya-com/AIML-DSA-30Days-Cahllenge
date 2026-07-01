95a8f70303c55c67d82e28780aabd06a5959fcac
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-01 21:31:43 +0530

    Remove unnecessary blank lines in student_attendance.py for improved readability

--- Day 26/student_attendance.py
+++ Day 26/student_attendance.py
@@ -2,15 +2,12 @@ attendance = { "Aman": 92,"Priya": 81, "Rahul": 65,"Neha": 98,"Riya": 74}
 
 def average_attendance(data):
     return sum(data.values()) / len(data)
-
 def highest_attendance(data):
     student = max(data, key=data.get)
     print("Highest Attendance:", student, "->", data[student])
-
 def lowest_attendance(data):
     student = min(data, key=data.get)
     print("Lowest Attendance:", student, "->", data[student])
-
 def above_average(data, avg):
     print("Students Above Average:")
     for student, att in data.items():
