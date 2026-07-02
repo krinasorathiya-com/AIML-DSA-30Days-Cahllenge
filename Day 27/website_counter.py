f789e890edc06d6ae3c0b88e764e1d82047bd37e
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-02 22:12:28 +0530

    Remove unnecessary blank lines in website counter script

--- Day 27/website_counter.py
+++ Day 27/website_counter.py
@@ -1,12 +1,9 @@
 visitors = ["India", "USA", "India","Canada",  "USA", "India", "Germany"]
-
 count = {}
-
 for country in visitors:
     if (country in count):
         count[country] += 1
     else:
         count[country] = 1
-
 for country, total in count.items():
     print(country, "->", total)
