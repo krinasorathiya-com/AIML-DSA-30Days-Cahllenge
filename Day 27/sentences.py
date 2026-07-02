d3a57aee9dc87f9541e94fae1cc5ace313bc4679
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-02 22:18:28 +0530

    Add sentence analysis functionality to count words and characters

---
+++ Day 27/sentences.py
@@ -0,0 +1,20 @@
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+
+total_words = len(words)
+total_characters = len(sentence)
+
+longest = words[0]
+shortest = words[0]
+
+for word in words:
+    if len(word) > len(longest):
+        longest = word
+
+    if len(word) < len(shortest):
+        shortest = word
+
+print("Total Words:", total_words)
+print("Total Characters:", total_characters)
+print("Longest Word:", longest)
+print("Shortest Word:", shortest)
