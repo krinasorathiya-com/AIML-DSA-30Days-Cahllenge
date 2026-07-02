13606f14b2a0fa35d12c8b5926e6ec7b8498b8d7
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-02 22:10:44 +0530

    Add website visitor counter script

---
+++ Day 27/website_counter.py
@@ -0,0 +1,20 @@
+visitors = [
+    "India",
+    "USA",
+    "India",
+    "Canada",
+    "USA",
+    "India",
+    "Germany"
+]
+
+count = {}
+
+for country in visitors:
+    if country in count:
+        count[country] += 1
+    else:
+        count[country] = 1
+
+for country, total in count.items():
+    print(country, "->", total)
