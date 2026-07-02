fb8fa662e4165d5d167d5c658623abe6b9bde917
krinasorathiya <krina.sorathiya4848@gmail.com>
2026-07-02 21:59:34 +0530

    Remove unnecessary blank lines in Employee.py

--- Day 27/Employee.py
+++ Day 27/Employee.py
@@ -11,9 +11,7 @@ for name, scores in employees.items():
     avg = calculate_average(scores)
     averages[name] = avg
     print(name, "Average Score:", round(avg, 2))
-
 best = max(averages, key=averages.get)
 lowest = min(averages, key=averages.get)
-
 print("\nBest Performer:", best)
 print("Lowest Performer:", lowest)
